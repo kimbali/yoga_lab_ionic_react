a41a4e7cbe0979245c7efdf704b2c73f1dd1f62c
etienne1980 <ducation.life@gmail.com>
2024-11-10 11:31:38 +0100

    add login page ionic component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
-import { login as loginService } from '../services/authService';
-import { useAuth } from '../contexts/AuthContext';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonButton,
+  IonCardContent,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonList,
+  IonButtons,
+  IonBackButton,
+} from "@ionic/react";
+import { logInOutline } from "ionicons/icons";
+import { login as loginService } from "../services/authService";
+import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
   const { login } = useAuth();
-  const [username, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError(''); // Clear previous errors
+    setError(""); // Clear previous errors
 
     try {
       const res = await loginService({ username, password });
-      console.log('Login successful:', res);
+      console.log("Login successful:", res);
 
       if (res.error) {
         setError(res.message);
@@ -24,37 +41,63 @@ const LoginPage: React.FC = () => {
 
       login(res.token);
     } catch (error: any) {
-      console.error('Login failed:', error);
+      console.error("Login failed:", error);
       setError(error.message); // Display the error message in the UI
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <label>
-        Username:
-        <input
-          type='text'
-          value={username}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type='password'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </label>
-      <button type='submit'>Login</button>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color={"primary"}>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={handleLogin}>
+              <IonList>
+                <IonItem>
+                  <IonInput
+                    className='ion-margin'
+                    fill='outline'
+                    labelPlacement='stacked'
+                    label='Email'
+                    type='email'
+                    placeholder='jonDoe@gmail.com'
+                    value={username}
+                    onIonChange={(e) => setEmail(e.detail.value!)}
+                  />
+                </IonItem>
+                <IonItem>
+                  <IonInput
+                    className='ion-margin'
+                    fill='outline'
+                    labelPlacement='stacked'
+                    label='Password'
+                    type='password'
+                    value={password}
+                    onIonChange={(e) => setPassword(e.detail.value!)}
+                  />
+                </IonItem>
+              </IonList>
 
-      {error && <p>Error: {error}</p>}
+              {error && <p>Error: {error}</p>}
 
-      <p>
-        Don't have an account? <Link to='/signup'>Sign up here</Link>
-      </p>
-    </form>
+              <IonButton type='submit' expand='block'>
+                Login
+                <IonIcon icon={logInOutline} slot='end' />
+              </IonButton>
+
+              <p>
+                Don't have an account? <Link to='/signup'>Sign up here</Link>
+              </p>
+            </form>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
