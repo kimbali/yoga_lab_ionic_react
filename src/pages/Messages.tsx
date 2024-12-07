28a2e93c23177a0d51a8e073a35da1d0aadf6d28
etienne1980 <ducation.life@gmail.com>
2024-12-07 11:16:18 +0100

    add separate css file

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -12,6 +12,7 @@ import {
 import { notificationsOffOutline } from "ionicons/icons";
 import Layout from "../components/Layout/Layout";
 import { getDeliveredNotifications } from "../services/pushNotificationService";
+import "./Messages.css"; // Import the external CSS file
 
 const Messages: React.FC = () => {
   const [notifications, setNotifications] = useState<string[]>([]);
@@ -20,7 +21,7 @@ const Messages: React.FC = () => {
     try {
       const delivered = await getDeliveredNotifications();
       const messages = delivered.notifications.map(
-          (n: any) => n.body || "No content"
+        (n: any) => n.body || "No content"
       );
       setNotifications(messages);
     } catch (error) {
@@ -30,41 +31,41 @@ const Messages: React.FC = () => {
   };
 
   return (
-      <Layout title="Push Notifications">
-        <IonGrid>
-          <IonRow className="ion-justify-content-center">
-            <IonCol size="12" size-md="8">
-              <IonCard>
-                <IonCardHeader style={{ textAlign: "center" }}>
-                  <h2>Notifications</h2>
-                </IonCardHeader>
-                <IonCardContent>
-                  {notifications.length > 0 ? (
-                      <ul>
-                        {notifications.map((note, index) => (
-                            <li key={index}>{note}</li>
-                        ))}
-                      </ul>
-                  ) : (
-                      <div style={{ textAlign: "center", color: "var(--ion-color-medium)" }}>
-                        <IonIcon
-                            icon={notificationsOffOutline}
-                            style={{ fontSize: "40px", marginBottom: "10px" }}
-                        />
-                        <p>No notifications to show</p>
-                      </div>
-                  )}
-                  <div style={{ textAlign: "center", marginTop: "20px" }}>
-                    <IonButton onClick={fetchNotifications} color="primary">
-                      Fetch Notifications
-                    </IonButton>
+    <Layout title="Push Notifications">
+      <IonGrid>
+        <IonRow className="ion-justify-content-center">
+          <IonCol size="12" size-md="8">
+            <IonCard>
+              <IonCardHeader className="card-header">
+                <h2>Notifications</h2>
+              </IonCardHeader>
+              <IonCardContent>
+                {notifications.length > 0 ? (
+                  <ul>
+                    {notifications.map((note, index) => (
+                      <li key={index}>{note}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <div className="no-notifications">
+                    <IonIcon
+                      icon={notificationsOffOutline}
+                      className="no-notifications-icon"
+                    />
+                    <p>No notifications to show</p>
                   </div>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </Layout>
+                )}
+                <div className="fetch-button-container">
+                  <IonButton onClick={fetchNotifications} color="primary">
+                    Fetch Notifications
+                  </IonButton>
+                </div>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </Layout>
   );
 };
 
