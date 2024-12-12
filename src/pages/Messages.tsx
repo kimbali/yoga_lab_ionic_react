86c15a24073d2858b442c90a9f4432055b0efc07
Kimbali <kimgarcianton@hotmail.com>
2024-12-12 09:13:34 +0100

    use global styles on messages

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   IonButton,
   IonCard,
@@ -8,11 +8,11 @@ import {
   IonRow,
   IonCol,
   IonIcon,
-} from "@ionic/react";
-import { notificationsOffOutline } from "ionicons/icons";
-import Layout from "../components/Layout/Layout";
-import { getDeliveredNotifications } from "../services/pushNotificationService";
-import "./Messages.css"; // Import the external CSS file
+} from '@ionic/react';
+import { notificationsOffOutline } from 'ionicons/icons';
+import Layout from '../components/Layout/Layout';
+import { getDeliveredNotifications } from '../services/pushNotificationService';
+import './Messages.css'; // Import the external CSS file
 
 const Messages: React.FC = () => {
   const [notifications, setNotifications] = useState<string[]>([]);
@@ -21,50 +21,49 @@ const Messages: React.FC = () => {
     try {
       const delivered = await getDeliveredNotifications();
       const messages = delivered.notifications.map(
-        (n: any) => n.body || "No content"
+        (n: any) => n.body || 'No content'
       );
       setNotifications(messages);
     } catch (error) {
-      console.error("Error fetching notifications:", error);
-      setNotifications(["Error fetching notifications"]);
+      console.error('Error fetching notifications:', error);
+      setNotifications(['Error fetching notifications']);
     }
   };
 
   return (
-    <Layout title="Push Notifications">
-      <IonGrid>
-        <IonRow className="ion-justify-content-center">
-          <IonCol size="12" size-md="8">
-            <IonCard>
-              <IonCardHeader className="card-header">
-                <h2>Notifications</h2>
-              </IonCardHeader>
-              <IonCardContent>
-                {notifications.length > 0 ? (
-                  <ul>
-                    {notifications.map((note, index) => (
-                      <li key={index}>{note}</li>
-                    ))}
-                  </ul>
-                ) : (
-                  <div className="no-notifications">
-                    <IonIcon
-                      icon={notificationsOffOutline}
-                      className="no-notifications-icon"
-                    />
-                    <p>No notifications to show</p>
-                  </div>
-                )}
-                <div className="fetch-button-container">
-                  <IonButton onClick={fetchNotifications} color="primary">
-                    Fetch Notifications
-                  </IonButton>
-                </div>
-              </IonCardContent>
-            </IonCard>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+    <Layout title='Push Notifications'>
+      <IonCard className='page-card'>
+        <IonCardHeader className='card-header'>
+          <h2>Notifications</h2>
+        </IonCardHeader>
+        <IonCardContent>
+          {notifications.length > 0 ? (
+            <ul>
+              {notifications.map((note, index) => (
+                <li key={index}>{note}</li>
+              ))}
+            </ul>
+          ) : (
+            <div className='no-notifications'>
+              <IonIcon
+                icon={notificationsOffOutline}
+                className='no-notifications-icon'
+              />
+              <p>No tienes notificaciones</p>
+            </div>
+          )}
+        </IonCardContent>
+        <IonButton
+          className='ion-margin'
+          color='primary'
+          size='large'
+          expand='block'
+          type='submit'
+          onClick={fetchNotifications}
+        >
+          Ver mensajes
+        </IonButton>
+      </IonCard>
     </Layout>
   );
 };
